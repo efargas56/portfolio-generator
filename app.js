9ad8afa9eeec48e336a229a6fa6490dc5dbf430b
Eduardo Fargas <efargas56@MacBook-Pro.attlocal.net>
2022-04-21 00:06:39 -0400

    working

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const profileDataArgs = process.argv.slice(2,process.argv.length);
+const fs = require('fs')
+const generatePage = require('./src/page-template')
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
 console.log(profileDataArgs);
 const printProfileData = profileDataArr => {
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
