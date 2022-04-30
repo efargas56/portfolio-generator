0ccaebe3e50af34c2e157db8df25057248cdacae
Eduardo Fargas <efargas56@Eduardos-MBP.attlocal.net>
2022-04-30 12:25:08 -0400

    FINISHED?

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -18,18 +18,20 @@ const writeFile = fileContent => {
      });
     }); 
 };
-const copyFile = cssFiles => {
+// copying file
+const copyFile = () => {
     return new Promise((resolve, reject) => {
-    fs.copyFile('./src/style.css', cssFiles, err => {
-    if (err) {
-        reject(err);
-        return;
-    }
-    resolve({
-        ok:true,
-        message: 'CSS copied!'
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        resolve({
+          ok: true,
+          message: 'Stylesheet created!'
+        });
+      });
     });
-});
-});
 };
 module.exports = { writeFile, copyFile };
